e028e522c1489066be77299a5928cb1213745ddd
SeanKeane6 <71730648+SeanKeane6@users.noreply.github.com>
2021-05-17 01:14:21 -0400

    Got Key
    Changed website but I don't know how to just access the music portion

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-console.log("connected");
+const key = "413585-Project-WV53SSDM";
+
+const query = async function() {
+    try {
+    const response = await fetch(
+        ``
+    );
+        const data = await response.json();
+        console.log(data);
+      }
+       catch (error) {
+        console.log(error);
+        alert("there was an error");
+      }
+    };
+query();
+
+// Still trying to figure out how to isolate the music section of the API
+
+//using TasteDive
